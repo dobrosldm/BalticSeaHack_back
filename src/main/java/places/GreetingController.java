070ff820221f1fdb65ab808199e0cd98b56c820a
dmitry <official.dobrosldm@gmail.com>
2019-11-09 11:35:41 +0300

    Initial commit

---
+++ src/main/java/places/GreetingController.java
@@ -0,0 +1,16 @@
+package places;
+
+import entities.Something;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("greeting")
+public class GreetingController {
+    @GetMapping(produces = "application/json")
+    public Something greeting() {
+        Something something = new Something();
+        something.setId(10);
+        something.setName("John");
+        return something;
+    }
+}
