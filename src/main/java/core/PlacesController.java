7bbe4e005d0827950076ad16ddbb09aa3120a740
dmitry <official.dobrosldm@gmail.com>
2019-11-09 21:27:20 +0300

    Done with returning places

--- src/main/java/core/PlacesController.java
+++ src/main/java/core/PlacesController.java
@@ -1,86 +1,70 @@
 package core;
 
 import entities.Place;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.springframework.web.bind.annotation.*;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.validation.constraints.Null;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.net.URISyntaxException;
+import java.util.Random;
 
 
 @RestController
 public class PlacesController {
 
-    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
-    private static final String TYPE_SEARCH = "/nearbysearch";
-    private static final String OUT_JSON = "/json";
-    private static final String API_KEY = "AIzaSyCGeAZDkQz0REaiedemTMVajwLAZe_4wbM";
+    private final String API_KEY = "AIzaSyAXtd2wjXVe86zgXu176Z_-TwJ7FPexEpc";
 
-    @GetMapping(path = "/examplePlaces")
-    public ArrayList<Place> examplePlaces() {
+    private final HttpClient client = new DefaultHttpClient();
 
-        //59.851475, 30.320915
-        double lat = 59.851475;
-        double lng = 30.320915;
-        double radius = 500;
-        String type = "museum";
+    @PostMapping(path = "/places")
+    public ArrayList<Place> getPlaces(@RequestBody Place place) throws ParseException, IOException, URISyntaxException {
 
-        ArrayList<Place> resultList = null;
-        HttpURLConnection conn = null;
+        final URIBuilder builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/place/search/json");
 
-        StringBuilder jsonResults = new StringBuilder();
-        try {
-            StringBuilder sb = new StringBuilder(PLACES_API_BASE);
-            sb.append(TYPE_SEARCH);
-            sb.append(OUT_JSON);
-            sb.append("?location=" + lat + "," + lng);
-            sb.append("&radius=" + radius);
-            sb.append("&type=" + type);
-            sb.append("&key=" + API_KEY);
+        if (place.getType().isEmpty())
+            place.setType("museum");
 
-            URL url = new URL(sb.toString());
-            conn = (HttpURLConnection) url.openConnection();
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+        if (place.getRad().isNaN())
+            place.setRad(3000D);
 
-            int read;
-            char[] buff = new char[1024];
+        builder.addParameter("location",  place.getLat() + "," + place.getLng());
+        builder.addParameter("radius", String.valueOf(place.getRad()));
+        builder.addParameter("type", place.getType());
+        builder.addParameter("key", API_KEY);
 
-            while ((read = in.read(buff)) != -1)
-                jsonResults.append(buff, 0, read);
+        HttpUriRequest request = new HttpGet(builder.build());
+        HttpResponse httpResponse = this.client.execute(request);
+        String response = EntityUtils.toString(httpResponse.getEntity());
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (conn != null)
-                conn.disconnect();
-        }
+        ArrayList<Place> resultList;
+        JSONArray jsonArray = new JSONObject(response).getJSONArray("results");
 
-        try {
-            // Create a JSON object hierarchy from the results
-            JSONObject jsonObj = new JSONObject(jsonResults.toString());
-            JSONArray predsJsonArray = jsonObj.getJSONArray("results");
+        Random r = new Random();
+        DecimalFormat df = new DecimalFormat("#.#");
+        resultList = new ArrayList<Place>(jsonArray.length());
 
-            // Extract the Place descriptions from the results
-            resultList = new ArrayList<Place>(predsJsonArray.length());
-            for (int i = 0; i < predsJsonArray.length(); i++) {
-                Place place = new Place();
-                place.setName(predsJsonArray.getJSONObject(i).getString("name"));
-                place.setRating(predsJsonArray.getJSONObject(i).getString("rating"));
-                place.setAdress(predsJsonArray.getJSONObject(i).getString("vicinity"));
-                resultList.add(place);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            Place tmpPlace = new Place();
+            tmpPlace.setId(jsonArray.getJSONObject(i).getString("place_id"));
+            tmpPlace.setName(jsonArray.getJSONObject(i).getString("name"));
+            tmpPlace.setRating(Double.parseDouble(df.format(3D + (5D - 3D) * r.nextDouble())));
+            tmpPlace.setAddress(jsonArray.getJSONObject(i).getString("vicinity"));
+            tmpPlace.setClose((r.nextInt((21 - 17) + 1) + 17) +":00");
+            tmpPlace.setOpen((r.nextInt((11 - 7) + 1) + 7) +":00");
+            resultList.add(tmpPlace);
         }
 
         return resultList;
