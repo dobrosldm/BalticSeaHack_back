959321f9d046a6786a9c0a3159bcea403a8d7bfe
Ars <ar.poryadin@yandex.ru>
2019-11-09 15:32:42 +0300

    swagger done

--- src/main/java/config/Swagger2Config.java
+++ src/main/java/config/Swagger2Config.java
@@ -1,7 +1,5 @@
 package config;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
