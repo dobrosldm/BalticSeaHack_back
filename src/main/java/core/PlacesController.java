fc6adc2b46d02ea4aa7db3bcb6ca199811aad683
dmitry <official.dobrosldm@gmail.com>
2019-11-09 15:21:16 +0300

    Working on Google Places API

--- src/main/java/core/PlacesController.java
+++ src/main/java/core/PlacesController.java
@@ -2,7 +2,6 @@ package core;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import se.walkercrou.places.GooglePlaces;
 import se.walkercrou.places.Place;
 
@@ -11,7 +10,9 @@ import java.util.List;
 @RestController
 public class PlacesController {
 
-    private GooglePlaces client = new GooglePlaces("AIzaSyCGeAZDkQz0REaiedemTMVajwLAZe_4wbM");
+    private static final String GOOGLE_API_KEY  = "AIzaSyCGeAZDkQz0REaiedemTMVajwLAZe_4wbM";
+
+    GooglePlaces client = new GooglePlaces(GOOGLE_API_KEY);
 
     @GetMapping(path = "/examplePlaces", produces = "application/json")
     public List<Place> examplePlaces() {
@@ -20,7 +21,7 @@ public class PlacesController {
         double lng = 30.320915;
         double radius = 500;
 
-        List<Place> places = client.getNearbyPlaces(lat, lng, radius, GooglePlaces.MAXIMUM_RESULTS);
+        List<Place> places = client.getNearbyPlaces(lat, lng, radius, GooglePlaces.DEFAULT_RESULTS);
 
         return places;
     }
