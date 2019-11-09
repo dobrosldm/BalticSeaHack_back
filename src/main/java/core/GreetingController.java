47d27ccbf39f92a1a05bb98ecbdc75a3caab79d3
Ars <ar.poryadin@yandex.ru>
2019-11-09 15:08:47 +0300

    swagger done

--- src/main/java/core/GreetingController.java
+++ src/main/java/core/GreetingController.java
@@ -1,16 +1,27 @@
 package core;
 
 import entities.Something;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("greeting")
+@Api(value="Greeting System", description="The most important one")
 public class GreetingController {
+    
     @GetMapping(produces = "application/json")
-    public Something greeting() {
+    @ApiOperation(value = "Returns a json of parametrs you've written in url", response = Something.class)
+    public Something greeting(@ApiParam(value = "Name, default - World", required = false)
+        @RequestParam(name="name", required=false, defaultValue="World") String name, 
+        @ApiParam(value = "Id", required = false)
+        @RequestParam(name="id", required=false) Integer id) {
+
         Something something = new Something();
-        something.setId(10);
-        something.setName("John");
+        something.setId(id);
+        something.setName(name);
         return something;
     }
 }
